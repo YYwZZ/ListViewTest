c8f544fe6f9b56de2f45295c5ee62e647a794d3d
YYwZZ <1195071851@qq.com>
2017-09-26 20:06:34 +0800

    fruitAdapter

--- app/src/main/java/app/wyyzzz/listviewtest/MainActivity.java
+++ app/src/main/java/app/wyyzzz/listviewtest/MainActivity.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     //Field
-    private String[] data = {"apple","banana","Orange","Watermelon","Pear","Grape","Pineapple","Strawberry","Cherry","Mango",
-            "apple","banana","Orange","Watermelon","Pear","Grape","Pineapple","Strawberry","Cherry","Mango"};
+    private List<Fruit> fruitList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,data);
-        ListView listView = (ListView)findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
+        
     }
 }
